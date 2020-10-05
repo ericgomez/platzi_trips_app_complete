15bd52e329c71970cd17a0664d81a2950fd7b5f4
Eric Gomez <eric.gomez.sanchez@gmail.com>
2020-10-05 11:25:02 -0500

    Cuarto Reto

--- lib/profile_trips.dart
+++ lib/profile_trips.dart
@@ -1,11 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:platzi_trips_app/review_list.dart';
+import 'description_place.dart';
+import 'header_appbar_profile.dart';
+import 'floating_action_button_profile.dart';
+
 
 class ProfileTrips extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Container(
-      color: Colors.indigo,
+    return Stack(//un Stack permite colocar un elemento encima de otro
+      children: [//En Stack tambien agregamos hijos
+        ListView(//Enviamos nuestro contenido en forma de lista para que podamos utilizar el scroll
+          children: [//Agrupamos los elemento
+            //ReviewList(),
+          ],
+        ),
+        HeaderAppBarProfile(),
+        FloatingActionButtonProfile()
+
+      ],
     );
   }
 
