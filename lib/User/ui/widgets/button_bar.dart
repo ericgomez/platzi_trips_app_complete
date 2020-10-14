29b74339635827770eb73c31c4b44f27e1a947c7
Eric Gomez <eric.gomez.sanchez@gmail.com>
2020-10-14 12:10:28 -0500

    Implementando Google SignOut en View

--- lib/User/ui/widgets/button_bar.dart
+++ lib/User/ui/widgets/button_bar.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:platzi_trips_app/User/bloc/bloc_user.dart';
 import 'circle_button.dart';
 
 class ButtonsBar extends StatelessWidget {
 
+  UserBloc userBloc;
+
   @override
   Widget build(BuildContext context) {
+    userBloc = BlocProvider.of(context);
     return Padding(
         padding: EdgeInsets.symmetric(
             horizontal: 0.0,
@@ -12,14 +17,19 @@ class ButtonsBar extends StatelessWidget {
         ),
         child: Row(
           children: [
-            CircleButton(true, Icons.turned_in_not, 20.0, Color.fromRGBO(255, 255, 255, 1)),
-            CircleButton(true, Icons.card_travel, 20.0, Color.fromRGBO(255, 255, 255, 0.6)),
+            CircleButton(true, Icons.turned_in_not, 20.0, Color.fromRGBO(255, 255, 255, 1), () => {}),
+            CircleButton(true, Icons.card_travel, 20.0, Color.fromRGBO(255, 255, 255, 0.6),() => {}),
             //Añadiremos un nuevo lugar
-            CircleButton(false, Icons.add, 40.0, Color.fromRGBO(255, 255, 255, 1)),
+            CircleButton(false, Icons.add, 40.0, Color.fromRGBO(255, 255, 255, 1),
+                    () => {}),
             //Cambiaremos la contraseña
-            CircleButton(true, Icons.vpn_key, 20.0, Color.fromRGBO(255, 255, 255, 0.6)),
+            CircleButton(true, Icons.vpn_key, 20.0, Color.fromRGBO(255, 255, 255, 0.6),
+                    () => {}),
             //Cerrar Session
-            CircleButton(true, Icons.exit_to_app, 20.0, Color.fromRGBO(255, 255, 255, 0.6))
+            CircleButton(true, Icons.exit_to_app, 20.0, Color.fromRGBO(255, 255, 255, 0.6),
+                    () => {
+                      userBloc.signOut()
+                    })
           ],
         )
     );
--- lib/platzi_trips_cupertino.dart
+++ lib/platzi_trips_cupertino.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
 import 'package:platzi_trips_app/Place/ui/screens/home_trips.dart';
 import 'package:platzi_trips_app/Place/ui/screens/search_trips.dart';
 import 'package:platzi_trips_app/Place/ui/screens/map_trips.dart';
 import 'package:platzi_trips_app/Place/ui/screens/notification_trips.dart';
+import 'package:platzi_trips_app/User/bloc/bloc_user.dart';
 import 'package:platzi_trips_app/User/ui/screens/profile_trips.dart';
 
 class PlatziTripsCupertino extends StatelessWidget {
@@ -68,7 +70,12 @@ class PlatziTripsCupertino extends StatelessWidget {
               break;
             case 4:
               return CupertinoTabView(
-                builder: (BuildContext context) => ProfileTrips(),
+                builder: (BuildContext context) {
+                  return BlocProvider<UserBloc>(
+                    bloc: UserBloc(),//Contentra los casos de uso
+                    child: ProfileTrips(),//Todos los Widget pueden tener acceso a los casos de uso que esten en bloc
+                  );
+                },
               );
               break;
 
