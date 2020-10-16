aa8140d08e274121f24af652bb82f795f0d70157
Eric Gomez <eric.gomez.sanchez@gmail.com>
2020-10-16 10:17:18 -0500

    Envío de datos de un fórmulario en Flutter

--- lib/User/repository/cloud_firestore_api.dart
+++ lib/User/repository/cloud_firestore_api.dart
@@ -1,4 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:platzi_trips_app/Place/model/place.dart';
 import 'package:platzi_trips_app/User/model/user.dart';
 
 class CloudFirestoreAPI {
@@ -9,11 +11,13 @@ class CloudFirestoreAPI {
   //Definimos nuestra entidad BD
   final Firestore _db =Firestore.instance;//Va a obtener toda la inicializacion o la conexion con la base de datos de la entidad de usuario
 
+  final FirebaseAuth _auth = FirebaseAuth.instance;//Esta variable contendra la instancia de FirebaseAuth
+
   //Creamos el metodo
   void updateUserData(User user) async {
     //Comenzamos a definir nuestra data
     DocumentReference ref = _db.collection(USERS).document(user.uid);
-    return ref.setData({
+    return await ref.setData({//Se ejecuta en segundo plano con await
       'uid': user.uid,
       'name': user.name,
       'email': user.email,
@@ -24,6 +28,22 @@ class CloudFirestoreAPI {
     },
         merge: true //Indicamos que aplique los cambios en la base de datos
     );
+  }
+
+  Future<void> updatePlace(Place place) async {
+    CollectionReference refPlaces = _db.collection(PLACES);//Generamos un identificados unico y auto incrementable
+
+    //Consultamos el usuario que esta logeado
+    await _auth.currentUser().then((FirebaseUser user) {
+      refPlaces.add({
+        //Optenemos los datos del formulario
+        'name': place.name,
+        'descripion': place.description,
+        'like': place.likes,
+        'userOwner': "${USERS}/${user.uid}"//Tipos de dato Reference
+      });
+    });
+
   }
 
 }
